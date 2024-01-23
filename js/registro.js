594cfdb91c3dc1e95014979cef2274c883bf01ca
Ivan <icarreras19961@iesjoanramis.org>
2024-01-23 23:21:51 +0700

    register y inicio """"echo""""

---
+++ js/registro.js
@@ -0,0 +1,29 @@
+// variables
+let registro = document.getElementById("boton_registro");
+let usuario = {
+  nombre: "",
+  email: "",
+  edad: "",
+  contrasena: "",
+  partidas: {
+    nivel_1: false,
+    nivel_2: false,
+    nivel_3: false,
+  },
+};
+// metodos
+
+// Insertar usuario si todo ba bien
+registro.addEventListener("click", (e) => {
+  e.preventDefault();
+  // la insercion de un usuario en el local storage
+  usuario.nombre = document.getElementById("nombre").value;
+  usuario.email = document.getElementById("email").value;
+  usuario.edad = document.getElementById("edad").value;
+  usuario.contrasena = document.getElementById("password").value;
+  console.log(usuario);
+
+  localStorage.setItem(usuario.email, JSON.stringify(usuario));
+  console.log(localStorage.getItem(usuario.email));
+  window.location.href = "html/menu_user.html";
+});
